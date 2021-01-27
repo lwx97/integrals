58bda238b88c4f00c9a0075255357de032a3c1ad
liwenxiang <liwenx97@163.com>
2021-01-27 22:16:29 +0800

    init

---
+++ src/main/java/com/maowudi/integrals/IntegralsApplication.java
@@ -0,0 +1,13 @@
+package com.maowudi.integrals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegralsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegralsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maowudi/integrals/IntegralsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maowudi.integrals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegralsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
